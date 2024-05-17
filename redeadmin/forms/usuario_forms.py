b316298756e78ea1de570749910dd4e5b3cd4ed6
cocogum <cdocogum@gmail.com>
2024-05-17 17:10:01 -0300

    Edição de usuarios funcionando

--- redeadmin/forms/usuario_forms.py
+++ redeadmin/forms/usuario_forms.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth import get_user_model
 
-class UsuarioForm(UserCreationForm):
+class CadastroUsuarioForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
-        
+
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False) 
         user.is_superuser = True
@@ -13,4 +13,11 @@ class UsuarioForm(UserCreationForm):
         if commit:
             user.save() 
         return user
+    
+class EditarUsuarioForm(UserChangeForm):
+    password:None
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'last_name', 'email']
             
--- redeadmin/urls.py
+++ redeadmin/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('servicos/editar/<int:id>', servicos_views.editar_servicos, name='editar_servicos'),
     path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
     path('usuarios/listar', usuario_views.listar_usuarios, name='cadastrar_usuarios'),
+    path('usuarios/editar/<int:id>', usuario_views.editar_usuario, name='editar_usuario'),
 ]
