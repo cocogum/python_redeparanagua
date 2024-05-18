72b43643a115ca1b0db61a1c1f89abaa4da1fe80
cocogum <cdocogum@gmail.com>
2024-05-18 11:10:11 -0300

    Modulo de autenticacao finalizado

--- redeadmin/views/servicos_views.py
+++ redeadmin/views/servicos_views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
 from ..forms.servicos_forms import ServicosForm
 from ..models import Servicos
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def cadastrar_servicos(request):
     if request.method == "POST":
         form_servicos = ServicosForm(request.POST)
@@ -15,10 +16,12 @@ def cadastrar_servicos(request):
         form_servicos = ServicosForm()
     return render(request, 'servicos/form_servicos.html', {"form_servicos": form_servicos})
 
+@login_required
 def listar_servicos(request):
     servicos = Servicos.objects.all()
     return render(request, 'servicos/lista_servicos.html', {"servicos": servicos})
 
+@login_required
 def editar_servicos(request, id):
     servicos = Servicos.objects.get(id=id)
     form_servicos = ServicosForm(request.POST or None, instance=servicos)
--- redeparanagua_admin/settings.py
+++ redeparanagua_admin/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = 'listar_servicos'
+
+LOGOUT_REDIRECT_URL = 'logar_usuario'
+
+LOGIN_URL = 'logar_usuario'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
